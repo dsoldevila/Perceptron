98a057ff2f7c194e482c9dc25f86ffb6c305b1ab
dsoldevila <david.sundevila@gmail.com>
2018-07-26 13:39:01 +0200

    It's something

---
+++ Neuron.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 26 13:27:31 2018
+
+@author: David
+"""
+
+class Neuron(): 
+    def __Init__(self, i, w):
+        self.inputs = i
+        self.weights = w
+        self.output = 0;
+        
+
+    
+        
